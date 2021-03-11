1bd381339d1cee9e6fb010b3f1b03521c4dae837
Marvin20212 <marvin.mueller@student.jade-hs.de>
2021-03-11 15:02:10 +0100

    Update

--- Hello World 3/Hello World 3/Program.cs
+++ Hello World 3/Hello World 3/Program.cs
@@ -10,7 +10,9 @@ namespace Hello_World_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello Meister");
+            Console.ReadKey();
+            Console.WriteLine("Hello Skrypni");
             Console.ReadKey();
         }
     }
